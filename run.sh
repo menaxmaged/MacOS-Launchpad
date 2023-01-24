c93a5233f3f2614aaaa4adad286cc17639090487
Mena Maged <menaxmaged@gmail.com>
2023-01-24 18:50:14 +0200

    SSSS

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 mkdir ~/.local/share/plasma/plasmoids/net.codexeg.plasma.maclauncher/
-cp -r ./* ~/.local/share/plasma/plasmoids/net.codexeg.plasma.maclauncher/
+cp -r ./package/* ~/.local/share/plasma/plasmoids/net.codexeg.plasma.maclauncher/
 plasmawindowed net.codexeg.plasma.maclauncher
